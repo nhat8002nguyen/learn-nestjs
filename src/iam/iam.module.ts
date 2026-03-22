e6362466173271b498221dd74395892db4159c7e
nhat.nguyen <nv.nhat8002@gmail.com>
2026-03-22 15:22:24 +0700

    refactor iam module

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -1,17 +1,47 @@
-import { forwardRef, Module } from "@nestjs/common";
-import { AuthenticationModule } from "./authentication/authentication.module";
+import { Module } from "@nestjs/common";
+import type { ConfigType } from "@nestjs/config";
+import { ConfigModule } from "@nestjs/config";
+import { APP_GUARD } from "@nestjs/core";
+import { JwtModule } from "@nestjs/jwt";
+import { CacheModule } from "src/cache/cache.module";
+import { UsersModule } from "src/users/users.module";
+import { AuthenticationController } from "./authentication/authentication.controller";
+import { AuthenticationService } from "./authentication/authentication.service";
+import jwtConfig from "./authentication/config/jwt.config";
+import { AccessTokenGuard } from "./authentication/guards/access-token.guard";
+import { JwtService } from "./authentication/jwt/jwt.service";
+import { RefreshTokenStorage } from "./authentication/refresh-token-storage/refresh-token-storage.service";
 import { BcryptService } from "./hashing/bcrypt/bcrypt.service";
 import { HashingService } from "./hashing/hashing.service";
 
 @Module({
+  imports: [
+    CacheModule,
+    UsersModule,
+    ConfigModule.forFeature(jwtConfig),
+    JwtModule.registerAsync({
+      imports: [ConfigModule.forFeature(jwtConfig)],
+      useFactory: (jwtConfiguration: ConfigType<typeof jwtConfig>) => ({
+        secret: jwtConfiguration.secret,
+      }),
+      inject: [jwtConfig.KEY],
+    }),
+  ],
+  controllers: [AuthenticationController],
   providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AccessTokenGuard,
+    },
     BcryptService,
     {
       provide: HashingService,
       useClass: BcryptService,
     },
+    AuthenticationService,
+    JwtService,
+    RefreshTokenStorage,
   ],
   exports: [HashingService],
-  imports: [forwardRef(() => AuthenticationModule)],
 })
 export class IamModule {}
